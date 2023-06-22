0a54decd898527d1eb5cd14a491ca5576fcefbfb
Kaesoron <103176244+Kaesoron@users.noreply.github.com>
2023-06-22 16:44:27 +0300

    Filter tests ok

--- src/test/java/cabinet/personal/other/HealthIndicators/testHealthIndicatorsDate.java
+++ src/test/java/cabinet/personal/other/HealthIndicators/testHealthIndicatorsDate.java
@@ -3,9 +3,7 @@ package cabinet.personal.other.HealthIndicators;
 import cabinet.CabinetLogIn;
 import cabinet.personal.PersonalCabinet;
 import cabinet.personal.other.OtherTab;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
@@ -34,36 +32,22 @@ public class testHealthIndicatorsDate {
         HealthIndicators.openingHealthIndicators(driver);
     }
 
-    @BeforeMethod
-    public void findBaseElements() {
-        WebElement fromDate = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[3]/div[1]"));
-        WebElement tillDate = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[3]/div[2]"));
-        WebElement clearDate = driver.findElement(By.xpath("//a[@class='clear']"));
+/*
+    Elements XPath for testing:
+ */
+//        WebElement fromDate = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[3]/div[1]"));
+//        WebElement tillDate = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[3]/div[2]"));
+//        WebElement clearDate = driver.findElement(By.xpath("//a[@class='clear']"));
+//
+//        WebElement firstEntryDateAndTime = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[1]/div[2]"));
+//        WebElement firstEntryMeasure = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[2]/div[2]"));
+//        WebElement firstEntryData = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[3]/div[2]/span"));
+//        WebElement firstEntryEditBtn = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[4]/div/a[1]"));
+//        WebElement firstEntryDeleteBtn = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[4]/div/a[2]"));
+//
+//        WebElement entryListQuantityCheckbox = driver.findElement(By.xpath("//*[@id=\"page-size-container\"]/p-dropdown/div/div[2]"));
 
-        WebElement firstEntryDateAndTime = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[1]/div[2]"));
-        WebElement firstEntryMeasure = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[2]/div[2]"));
-        WebElement firstEntryData = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[3]/div[2]/span"));
-        WebElement firstEntryEditBtn = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[4]/div/a[1]"));
-        WebElement firstEntryDeleteBtn = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[4]/div/a[2]"));
 
-        WebElement entryListQuantityCheckbox = driver.findElement(By.xpath("//*[@id=\"page-size-container\"]/p-dropdown/div/div[2]"));
-    }
-
-    @BeforeMethod
-    public void findElements() {
-        WebElement indicatorFilterType = driver.findElement(By.xpath("//div[@class='indicator-filter']"));
-//        WebElement clearFilter = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[2]/i"));
-//        WebElement indicatorFilterTemperature = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[1]"));
-//        WebElement indicatorFilterWeight = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[2]"));
-//        WebElement indicatorFilterPressure = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[3]"));
-//        WebElement indicatorFilterSugar = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[4]"));
-//        WebElement indicatorFilterPulse = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[5]"));
-//        WebElement indicatorFilterMood = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[6]"));
-//        WebElement indicatorFilterAlcohol = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[7]"));
-//        WebElement indicatorFilterAmbivalence = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[8]"));
-//        WebElement indicatorFilterHealthStatus = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[9]"));
-//        WebElement indicatorFilterSkin = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[10]"));
-    }
 
 
     public void test1() {
--- src/test/java/cabinet/personal/other/HealthIndicators/testHealthIndicatorsFilter.java
+++ src/test/java/cabinet/personal/other/HealthIndicators/testHealthIndicatorsFilter.java
@@ -7,8 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -36,40 +36,56 @@ public class testHealthIndicatorsFilter {
         HealthIndicators.openingHealthIndicators(driver);
     }
 
-    @BeforeMethod
-    public void findBaseElements() {
-        WebElement fromDate = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[3]/div[1]"));
-        WebElement tillDate = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[3]/div[2]"));
-        WebElement clearDate = driver.findElement(By.xpath("//a[@class='clear']"));
 
-        WebElement firstEntryDateAndTime = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[1]/div[2]"));
-        WebElement firstEntryMeasure = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[2]/div[2]"));
-        WebElement firstEntryData = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[3]/div[2]/span"));
-        WebElement firstEntryEditBtn = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[4]/div/a[1]"));
-        WebElement firstEntryDeleteBtn = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[3]/div/div[4]/div/a[2]"));
-
-        WebElement entryListQuantityCheckbox = driver.findElement(By.xpath("//*[@id=\"page-size-container\"]/p-dropdown/div/div[2]"));
-    }
-
-    @BeforeMethod
-    public void findElements() {
-        WebElement indicatorFilterType = driver.findElement(By.xpath("//div[@class='indicator-filter']"));
+/*
+    Elements XPath for testing:
+*/
 //        WebElement clearFilter = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[2]/i"));
-        WebElement indicatorFilterTemperature = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[1]"));
-        WebElement indicatorFilterWeight = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[2]"));
-        WebElement indicatorFilterPressure = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[3]"));
-        WebElement indicatorFilterSugar = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[4]"));
-        WebElement indicatorFilterPulse = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[5]"));
-        WebElement indicatorFilterMood = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[6]"));
-        WebElement indicatorFilterAlcohol = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[7]"));
-        WebElement indicatorFilterAmbivalence = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[8]"));
-        WebElement indicatorFilterHealthStatus = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[9]"));
-        WebElement indicatorFilterSkin = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[10]"));
-    }
+//        WebElement indicatorFilterTemperature = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[1]"));
+//        WebElement indicatorFilterWeight = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[2]"));
+//        WebElement indicatorFilterPressure = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[3]"));
+//        WebElement indicatorFilterSugar = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[4]"));
+//        WebElement indicatorFilterPulse = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[5]"));
+//        WebElement indicatorFilterMood = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[6]"));
+//        WebElement indicatorFilterAlcohol = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[7]"));
+//        WebElement indicatorFilterAmbivalence = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[8]"));
+//        WebElement indicatorFilterHealthStatus = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[9]"));
+//        WebElement indicatorFilterSkin = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[10]"));
 
     @Test(description="Filter options clickable")
     public void filterOptionClickable() {
-
+        WebElement indicatorFilterType = driver.findElement(By.xpath("//div[@class='indicator-filter']"));
+        Assert.assertTrue(indicatorFilterType.isEnabled());
     }
 
+    @Test(description="Can select filter option")
+    public void filterTemperatureClickable() {
+        WebElement indicatorFilterType = driver.findElement(By.xpath("//div[@class='indicator-filter']"));
+        indicatorFilterType.click();
+        WebElement indicatorFilterTemperature = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[1]"));
+        indicatorFilterTemperature.click();
+    }
+
+    @Test(description="Temperature filter works")
+    public void filterTemperatureWorks() {
+        WebElement indicatorFilterType = driver.findElement(By.xpath("//div[@class='indicator-filter']"));
+        indicatorFilterType.click();
+        WebElement indicatorFilterTemperature = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[1]"));
+        indicatorFilterTemperature.click();
+        WebElement entryType = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[2]/div[2]/div/div[2]/div[2]"));
+        Assert.assertEquals(entryType.getText(), "Температура");
+    }
+
+    @Test(description = "Cal clear filter selection")
+    public void filterClear() {
+        WebElement indicatorFilterType = driver.findElement(By.xpath("//div[@class='indicator-filter']"));
+        indicatorFilterType.click();
+        WebElement indicatorFilterTemperature = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[4]/div/ul/p-dropdownitem[1]"));
+        indicatorFilterTemperature.click();
+        WebElement clearFilter = driver.findElement(By.xpath("/html/body/app-root/div/app-account/main/app-patient-card/main/div/app-health-indicators/main/div[1]/div/div[1]/div/div[2]/p-dropdown/div/div[2]/i"));
+        clearFilter.click();
+    }
+
+
+    //The same for other filter options
 }
